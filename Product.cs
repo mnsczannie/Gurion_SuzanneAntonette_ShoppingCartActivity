927a3dd7f1c4b95c7eddd6621a8dab53e5b10d4d
mnsczannie <zanneantonette09@gmail.com>
2026-04-11 05:40:08 +0800

    steps 1-12 should be here (didn't see the "In main()") hihi

--- Product.cs
+++ Product.cs
@@ -8,49 +8,20 @@ namespace _1_ShoppingCartActivity
 {
     internal class Product
     {
+
         /* fields/properties
          *  ID
          *  Name
          *  Price
          *  Remaining Stock
          */
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int RemainingStock { get; set; }
 
-        /* Store Menu:
-         * -products
-         *  add products
-         *      product number
-         *      price
-         *      stock
-         *  remove products
-         *  display products
-         * -orders
-         *  display products
-         *  Enter product number 
-         *      validate: not existing, wrong input, duplicate (if yes, update the existing)
-         *  Enter quantity
-         *      validate: negative, out of stock, not a number
-         *  add to cart
-         *      (fixed-size cart) full = 20 items
-         *      or
-         *      keep going util user is done
-         *  show grand total
-         * -Checkout
-         *  display cart items
-         *  display grand total
-         *  enter discount code
-         *      (if grand total >= 5000) 10% discount
-         *  display final total
-         */
 
-        /*Need:
-         validation messages:
-            - added/removed product
-            - invalid product number
-            - invalid quantity
-            - added to cart
-                    - out of stock
-            - "are you sure you want to checkout? (y/n)"
-            - "thank you for shopping with us!"
-        */
+
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Store Menu:
+         * -products
+         *  add products
+         *      product number
+         *      price
+         *      stock
+         *  remove products
+         *  display products
+         * -orders
+         *  display products
+         *  Enter product number 
+         *      validate: not existing, wrong input, duplicate (if yes, update the existing)
+         *  Enter quantity
+         *      validate: negative, out of stock, not a number
+         *  add to cart
+         *      (fixed-size cart) full = 20 items
+         *      or
+         *      keep going util user is done
+         *  show grand total
+         * -Checkout
+         *  display cart items
+         *  display grand total
+         *  enter discount code
+         *      (if grand total >= 5000) 10% discount
+         *  display final total
+         */
+
+/*Need:
+ validation messages:
+    - added/removed product
+    - invalid product number
+    - invalid quantity
+    - added to cart
+            - out of stock
+    - "are you sure you want to checkout? (y/n)"
+    - "thank you for shopping with us!"
+*/
