b60366d0adcf099da09ad2ed277aa3d8e3861a13
mnsczannie <zanneantonette09@gmail.com>
2026-04-11 04:11:03 +0800

    flow :DD

--- Product.cs
+++ Product.cs
@@ -8,6 +8,39 @@ namespace _1_ShoppingCartActivity
 {
     internal class Product
     {
-        //josko natapos din magayos ng file at commit
+        /* fields/properties
+         *  ID
+         *  Name
+         *  Price
+         *  Remaining Stock
+         */
+
+        /* Menu:
+         * -products
+         *  add products
+         *      product number
+         *      price
+         *      stock
+         *  remove products
+         *  display products
+         * -orders
+         *  display products
+         *  Enter product number 
+         *      validate: not existing, wrong input, duplicate (if yes, update the existing)
+         *  Enter quantity
+         *      validate: negative, out of stock, not a number
+         *  add to cart
+         *      (fixed-size cart) full = 20 items
+         *      or
+         *      keep going util user is done
+         *  show grand total
+         * -Checkout
+         *  display cart items
+         *  display grand total
+         *  enter discount code
+         *      (if grand total >= 5000) 10% discount
+         *  display final total
+         */
+
     }
 }
